7e58f11f743155a8cd8c25f768870d1a0668ec75
Mobiso <95570641+Mobiso@users.noreply.github.com>
2023-04-20 11:14:28 +0200

    Ragdoll With Movement
    Added A physics based ragdoll with movement. Controlled by a cube above the player.

---
+++ Assets/Models/PlayerModel/PlayerRagdollScripts/CubeControl.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControl : MonoBehaviour
+{
+    public Rigidbody cube;
+    public float MovementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+
+        //changes velocity of RigidBody rb
+          if(moveDirection != Vector3.zero){
+            cube.transform.forward = moveDirection;
+        }
+
+        //changes velocity of RigidBody rb
+        cube.AddForce(moveDirection*MovementSpeed);
+    }
+}
