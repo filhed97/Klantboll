2ae4ad17a1027927244b2bccb4bc9677e9ae7864
Mostafa Aziz Zuher <thecool4a@gmail.com>
2023-04-25 14:05:41 +0200

    Correct powerup added

--- Assets/Scripts/PowerUp Scripts/Powerup.cs
+++ Assets/Scripts/PowerUp Scripts/Powerup.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class Powerup : MonoBehaviour
 {
+    public float duration = 4f;
+    public float defaultspeed = 500;
     public PowerupEffects powerupEffects;
 
     private void OnTriggerEnter(Collider other)
     {
-       if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            Destroy(gameObject);
-            powerupEffects.Apply(other.gameObject);
+            StartCoroutine(Pickup(other));
         }
+    }
+
+    IEnumerator Pickup(Collider other)
+    {
+        gameObject.GetComponent<Collider>().enabled = false;
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+
+        powerupEffects.Apply(other.gameObject);
+        yield return new WaitForSeconds(duration);
+
+        other.gameObject.GetComponent<PlayerMove>().speed = defaultspeed;
+        other.gameObject.GetComponent<PlayerMove>().strafeSpeed = defaultspeed;
+        Destroy(gameObject);
+
     }
 }
