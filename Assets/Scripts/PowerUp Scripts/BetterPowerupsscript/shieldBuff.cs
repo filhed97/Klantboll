400d2e4bdcd9592e3344d5a9efac69eeadde552b
Mahdi Nazari <100143248+MahdiNazarii@users.noreply.github.com>
2023-05-04 16:14:08 +0200

    Shield power-up spawns on the field and can be picked up by the AI character

--- Assets/Scripts/PowerUp Scripts/BetterPowerupsscript/shieldBuff.cs
+++ Assets/Scripts/PowerUp Scripts/BetterPowerupsscript/shieldBuff.cs
@@ -25,6 +25,20 @@ public class shieldBuff : PowerupEffects2
                 shieldInstance = Instantiate(shieldPrefab, shieldPosition, shieldRotation, hips.transform);
             }
         }
+        else
+        {
+            target.GetComponent<AIScript>().hasPowerup = true;
+        
+            if (shieldInstance == null)
+            {
+                Rigidbody hips = target.GetComponent<Rigidbody>();
+                float shieldDistance = 2f; 
+                float shieldHeightOffset = 2f;
+                Vector3 shieldPosition = hips.transform.position + hips.transform.forward * shieldDistance + Vector3.up * shieldHeightOffset;
+                Quaternion shieldRotation = hips.transform.rotation * Quaternion.Euler(90, 0, 0);
+                shieldInstance = Instantiate(shieldPrefab, shieldPosition, shieldRotation, hips.transform);
+            }
+        }
     }
 
     public override void remove(GameObject target)
@@ -32,6 +46,16 @@ public class shieldBuff : PowerupEffects2
         if(target.CompareTag("Player"))
         {
             target.GetComponent<PlayerMove>().hasPowerup = false;
+            
+            if (shieldInstance != null)
+            {
+                Destroy(shieldInstance);
+                shieldInstance = null;
+            }  
+        }
+        else 
+        {
+            target.GetComponent<AIScript>().hasPowerup = false;
             
             if (shieldInstance != null)
             {
